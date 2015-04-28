77f6774ade76409dc85a29dd775cd0deb2f5a667
Vit <Habada>
2015-04-28 17:10:54 +0200

    password recovery

--- app/FrontModule/presenters/SignPresenter.php
+++ app/FrontModule/presenters/SignPresenter.php
@@ -3,15 +3,44 @@
 namespace App\FrontModule\Presenters;
 
 
+use App\Model\Entities\PasswordRecovery;
+use App\Model\Entities\User;
 use App\Model\Repositories\UserRepository;
 use Doctrine\DBAL\Exception\UniqueConstraintViolationException;
+use Nette\Application\BadRequestException;
 use Nette\Application\UI\Form;
+use Nette\Mail\IMailer;
+use Nette\Mail\Message;
+use Nette\Utils\ArrayHash;
 
 class SignPresenter extends BasePresenter
 {
+    /** @var IMailer @inject */
+    public $mailer;
 
     /** @var UserRepository @inject */
     public $userRepository;
+
+    public function actionRecovery($id)
+    {
+        /** @var PasswordRecovery $rec */
+        $rec = $this->em->getRepository(PasswordRecovery::class)->findOneBy(
+            [
+                'token' => $id,
+                'valid' => TRUE,
+            ]);
+        if (!$rec) throw new BadRequestException;
+
+        if (!$rec->isAlive()) {
+            $this->flashMessage('Platnost tohoto odkazu vypršela (14 dní).', 'warning');
+            $this->redirect('Sign:recover');
+        }
+
+        $this['recoveryForm']['token']->setDefaultValue($rec->getToken());
+
+        $this->template->step3 = TRUE;
+        $this->setView('recover');
+    }
 
     /**
      * [RegisterForm]
@@ -42,9 +71,33 @@ class SignPresenter extends BasePresenter
      */
     public function recoverFormSucceeded(Form $form, $values)
     {
-        // TODO recovery function
-        dump($values);
-        die;
+        // validate user
+        /** @var User $user */
+        $user = $this->em->getRepository(User::class)->findOneBy(['email' => $values->email]);
+        if (!$user) {
+            $form->addError('Uživatel s tímto e-mailem neexistuje.');
+            return;
+        }
+
+        // create password recovery
+        $rec = new PasswordRecovery($values->email);
+        $this->em->persist($rec);
+        $this->em->flush();
+
+        // send e-mail
+        $mail = new Message;
+        $mail->setFrom('MFFun <noreply@beinshop.cz>')
+            ->addTo($rec->getEmail())
+            ->setSubject('Obnova hesla')
+            ->setHtmlBody("
+            Dobrý den,<br>
+            na tuto e-mailovou adresu byl vyžádán odkaz pro obnovu hesla k účtu na MFFun. Pokud si chcete obnovit heslo klikněte na odkaz níže (platnost odkazu je 14 dní).
+            <br><br>
+            <a href=\"{$this->link('Sign:recovery', $rec->getToken())}\">{$this->link('Sign:recovery', $rec->getToken())}</a>
+            ");
+        $this->mailer->send($mail);
+
+        $this->template->step2 = true;
     }
 
     /**
@@ -62,6 +115,50 @@ class SignPresenter extends BasePresenter
             ->getControlPrototype()->class('form-input');
         $form->addSubmit('process', 'Odeslat')
             ->getControlPrototype()->class('button blue');
+
+        $form->onSuccess[] = $this->recoverFormSucceeded;
+        $form->addProtection();
+
+        return $form;
+    }
+
+    /**
+     * RecoveryForm factory.
+     *
+     * @return Form
+     */
+    protected function createComponentRecoveryForm()
+    {
+        $form = new Form();
+
+        $form->addPassword('password', 'Nové heslo')
+            ->addRule(Form::MIN_LENGTH, 'Zadejte aspoň 6 znaků.', 6)
+            ->setRequired('Vyplňte prosím.');
+        $form->addPassword('password2', 'Heslo znovu')
+            ->setRequired('Vyplňte prosím.')
+            ->addRule(Form::EQUAL, 'Hesla se neshodují.', $form['password'])
+            ->setOmitted();
+        $form->addHidden('token', '');
+
+        foreach ($form->getControls() as $control) {
+            $control->getControlPrototype()->class('form-input');
+        }
+
+        $form->addSubmit('process', 'Registrovat')
+            ->getControlPrototype()->class('button blue');
+
+        $form->onSuccess[] = function (Form $form, ArrayHash $values) {
+            /** @var PasswordRecovery $rec */
+            $rec = $this->em->getRepository(PasswordRecovery::class)->findOneBy(['token' => $values->token]);
+            $rec->invalidate();
+            $user = $this->em->getRepository(User::class)->findOneBy(['email' => $rec->getEmail()]);
+            $user->setPassword($values->password);
+            $this->em->flush();
+
+            $this->redirect('Homepage:default');
+            $this->flashMessage('Vaše heslo bylo změněno.', 'info');
+        };
+        $form->addProtection();
 
         return $form;
     }
@@ -89,8 +186,6 @@ class SignPresenter extends BasePresenter
             ->addRule(Form::EQUAL, 'Hesla se neshodují.', $form['password'])
             ->setOmitted();
         $form->addText('name', 'Jméno');
-
-        // TODO avatar upload
 
         foreach ($form->getControls() as $control) {
             $control->getControlPrototype()->class('form-input');
---
+++ app/model/entities/PasswordRecovery.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Model\Entities;
+
+use Doctrine\ORM\Mapping as ORM;
+use Kdyby\Doctrine\Entities\BaseEntity;
+
+/**
+ * @ORM\Entity
+ */
+class PasswordRecovery extends BaseEntity
+{
+    /**
+     * Days for token to be alive.
+     */
+    const TOKEN_TTL = 14;
+
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @var \DateTime
+     */
+    private $created;
+
+    /**
+     * @ORM\Column(type="string", unique=true)
+     * @var string
+     */
+    private $token;
+
+    /**
+     * @ORM\Column(type="string")
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean")
+     * @var bool
+     */
+    private $valid = TRUE;
+
+    /**
+     * @param $email
+     */
+    public function __construct($email)
+    {
+        $this->created = new \DateTime();
+        $this->token = $this->generateToken();
+        $this->email = $email;
+    }
+
+    private function generateToken()
+    {
+        // TODO better algo
+        return sha1(microtime());
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * @param \DateTime $created
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return boolean
+     */
+    public function isValid()
+    {
+        return $this->valid;
+    }
+
+    public function invalidate()
+    {
+        $this->valid = FALSE;
+    }
+
+    public function isAlive()
+    {
+        $now = new \DateTime();
+        $diff = $this->created->diff($now);
+
+        return ($diff->days <= self::TOKEN_TTL);
+    }
+
+
+}
