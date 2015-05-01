f0d385f517554dfd2d9c71021d207b52ddf09d39
Vit Habada <vithabada93@gmail.com>
2015-05-01 13:59:21 +0200

    mail config

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,6 +15,7 @@ $configurator->createRobotLoader()
 $configurator->addConfig(__DIR__ . '/config/config.neon');
 $configurator->addConfig(__DIR__ . '/config/config.local.neon');
 $configurator->addConfig(__DIR__ . '/config/services.neon');
+$configurator->addConfig(__DIR__ . '/config/mailer.neon');
 
 $configurator->onCompile[] = function ($configurator, $compiler) {
     $compiler->addExtension('thumbnail', new \Kollarovic\Thumbnail\DI\Extension);
