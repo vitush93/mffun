28047d93ead77e74bf2804523e3127523014a656
Vit Habada <vithabada93@gmail.com>
2015-05-01 10:04:45 +0200

    ajax reload after time out

--- www/dev/js/scrollLoad.js
+++ www/dev/js/scrollLoad.js
@@ -7,40 +7,63 @@ var ScrollLoad = function (container, loader, moreButton, endmsg, bottomOffset, 
     this.loader = loader;
     this.moreButton = moreButton;
     this.endmsg = endmsg;
+    this.reloadButtonContent = $('#reload-button-content');
     this.init();
 };
 
 ScrollLoad.prototype = {
 
     load: function (page) {
-        var $loader = $(this.loader);
-        var $more = $(this.moreButton);
-        var $container = $(this.container);
-        var $endmsg = $(this.endmsg);
         var context = this;
         $.ajax({
             type: 'GET',
             url: window.location.toString(),
-            data: 'do=load&page=' + page
-        }).done(function (data) {
-            $container.append(data);
-            if (data.more) {
-                $loader.hide();
-                $more.show();
-                context.lock = true;
-            } else if (data.nomore) {
-                $loader.hide();
-                $endmsg.show();
-                context.lock = true;
-            } else {
-                context.lock = false;
-                context.currentPage++;
+            data: 'do=load&page=' + page,
+            timeout: 5000,
+            error: function (x, t, m) {
+                $(context.loader).hide();
+                context.reloadButtonContent.show();
+                context.reload(this);
             }
+        }).done(function (data) {
+            context.appendData(data);
+        });
+    },
+
+    appendData: function (data) {
+        var $loader = $(this.loader);
+        var $more = $(this.moreButton);
+        var $container = $(this.container);
+        var $endmsg = $(this.endmsg);
+        $container.append(data);
+        if (data.more) {
+            $loader.hide();
+            $more.show();
+            this.lock = true;
+        } else if (data.nomore) {
+            $loader.hide();
+            $endmsg.show();
+            this.lock = true;
+        } else {
+            this.lock = false;
+            this.currentPage++;
+        }
+    },
+
+    reload: function (ajaxContext) {
+        var context = this;
+        $('body').on('click', '#reload-button', function () {
+            context.reloadButtonContent.hide();
+            $(context.loader).show();
+            $.ajax(ajaxContext).done(function (data) {
+                context.appendData(data);
+            });
         });
     },
 
     init: function () {
         var context = this;
+
         setInterval(function () {
             if (context.lock) return;
             var diff = document.body.offsetHeight - (window.innerHeight + window.scrollY);
