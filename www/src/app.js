04722638100cfe8bf4989aaf18c57bb54e97502f
Vit Habada <vithabada93@gmail.com>
2015-10-27 01:28:29 +0100

    backbone router firing twice fix

--- www/src/app.js
+++ www/src/app.js
@@ -7,18 +7,7 @@ var Backbone = require('backbone');
 var AppRouter = require('./router/AppRouter');
 
 $(function () {
-    var router = new AppRouter();
-
-    //Backbone.history.start({
-    //    pushState: true,
-    //    root: '/'
-    //});
+    new AppRouter();
 
     Backbone.history.start();
-
-    var url = window.location.pathname;
-    //url = url.substr(1);
-
-    //router.navigate(url, {trigger: true});
-    router.navigate(url);
 });
